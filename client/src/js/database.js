27e8d89b86a8c1b3f5c924c204c039a21969118b
Ricky Thakar <rickythakar@gmail.com>
2022-06-06 15:07:54 -0400

    worked on src-sw js and pulled code from class activity

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,6 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-console.error('putDb not implemented'); 
 const jateDb = await openDB('jate', 1);   
 const tx = jateDb.transaction('jate', 'readwrite');   
 const store = tx.objectStore('jate');   
@@ -24,7 +23,6 @@ const result = await request;   console.log('🚀 - data saved to the database',
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
   console.log("this is the get");
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
